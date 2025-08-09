5113ad083ccafdf3646b54552c33288352a063b3
Lauriely Lourenço <laurielylourenco@gmail.com>
2025-08-09 14:28:21 -0300

    fix:comandos de permissão de pasta

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -10,6 +10,10 @@ fi
 
 echo "Rodando as migrações do banco de dados..."
 php artisan migrate --force
+   
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 # Executa o comando principal do contêiner (php-fpm)
 exec "$@"
